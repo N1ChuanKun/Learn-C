f3de7eccc219da63bd1bf2440965a8a23603fb69
Kun <95230582+N1ChuanKun@users.noreply.github.com>
2022-03-02 23:09:48 +0900

    Create sort.c

---
+++ Data Structures in C/part1/sort.c
@@ -0,0 +1,35 @@
+//선택 정렬
+#include<stdio.h>
+#include<math.h>
+#define SWAP(x,y,t) ((t)=(x),(x)=(y),(y)=(t))
+
+void sort(int* arr, int size);
+
+int main() {
+	int* arr;
+	int n, i;
+
+	printf("Input size of array: ");
+	scanf("%d", &n);
+	arr = (int*)malloc(sizeof(int) * n);
+
+	for (i = 0; i < n; i++) {
+		arr[i] = rand() % 1000;
+		printf("%d ", arr[i]);
+	}
+	sort(arr, n);
+	printf("\nSorted array: ");
+	for (i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+}
+
+void sort(int *arr, int size) {
+	int i, j, min, temp;
+	for (i = 0; i < size - 1; i++) {
+		min = i;
+		for (j = i + 1; j < size; j++)
+			if (arr[i] > arr[j])
+				min = j;
+		SWAP(arr[i], arr[min], temp);
+	}
+}
